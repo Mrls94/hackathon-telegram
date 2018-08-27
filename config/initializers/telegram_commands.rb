681c20bc80def5ebe8a95dc184109f769bf853a4
Hans Gamarra <hans.gamarra1303@gmail.com>
2018-08-26 22:57:40 -0500

    fix for parsing arguments

--- config/initializers/telegram_commands.rb
+++ config/initializers/telegram_commands.rb
@@ -1,5 +1,15 @@
 require 'telegram/commander'
 
-Telegram::Commander.add_command(:createBoard) do |client, command|
-  client.send_message(text: "Let\'s start by setting your Trello board. Please send the board ID", body: {})
+Telegram::Commander.add_command(:create_board) do |client|
+  client.send_message(text: "First things first: Set your Trello board by using the command `/trello_board_id <board_id>`", body: {})
+end
+
+Telegram::Commander.add_command(:trello_board_id) do |client, command|
+  client.send_message(text: "Great! I got your board id: #{command.args.first}", body: {})
+  client.send_message(text: "Now I need your Trello Access Token. Please use `/trello_access_token <access_token>`", body: {})
+end
+
+Telegram::Commander.add_command(:trello_access_token) do |client, command|
+  client.send_message(text: "Great! I got your access_token: #{command.args.first}", body: {})
+  client.send_message(text: "Creating necessary columns...", body: {})
 end
